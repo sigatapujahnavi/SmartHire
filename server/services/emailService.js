3296ae8f26bd54595e3b5f66496a25a40ee52c20
Jahnavi Sigatapu <sigatapujahnavi77@gmail.com>
2026-05-25 11:53:14 +0530

    Switch to Brevo SMTP

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -2,13 +2,14 @@
 import nodemailer from 'nodemailer'
 
 export const sendOTPEmail = async (email, fullName, otp) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  })
+const transporter = nodemailer.createTransport({
+  host: 'smtp-relay.brevo.com',
+  port: 587,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+})
 
   await transporter.sendMail({
     from: `"SmartHire" <${process.env.EMAIL_USER}>`,
